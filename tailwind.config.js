cc45c0c00d40cfd84a588e92b9a9d48f866cdcdb
darien rosero <darien.rosero2003@gmail.com>
2024-06-03 15:26:21 -0500

    {inicio}: configuracion y creacion de funte y colores custom

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'grey-blue': '#1e213a',
+        'dark-blue': '#100e1d',
+        'light-bluw': '#3c47e9',
+        'grey-R': '#6e707a',
+        'grey-L': '#a09fb1',
+        'grey-F': '#585676',
+        'light-yeloow': '#ffec65',
+        'white-R': '#e7e7eb'
+      },
+      fontFamily:{
+         "Raleway": [ 'Raleway', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
